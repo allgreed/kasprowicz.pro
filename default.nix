2e628b3d82b4a292b0961e4994e9c05e35ecd496
Olgierd "Allgreed" Kasprowicz <olgierd@kasprowicz.pro>
2021-08-16 22:53:56 +0200

    Pin nixpkgs

--- default.nix
+++ default.nix
@@ -1,9 +1,15 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-with pkgs;
-
-mkShell {
-  buildInputs = [
+let
+  nixpkgs = builtins.fetchGit {
+    name = "nixos-unstable-2021-03-17";
+    url = "https://github.com/nixos/nixpkgs/";
+    ref = "refs/heads/nixos-unstable";
+    rev = "266dc8c3d052f549826ba246d06787a219533b8f";
+    # obtain via `git ls-remote https://github.com/nixos/nixpkgs nixos-unstable`
+  };
+  pkgs = import nixpkgs { config = {}; };
+in
+pkgs.mkShell {
+  buildInputs = with pkgs; [
     hugo
     git
     gnumake 
